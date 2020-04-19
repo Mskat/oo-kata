15ffa10766895d2f523b9d34ec2bf323c11c6863
zur.katarzyna <zur.katarzyna@gmail.com>
2020-04-19 21:48:27 +0100

    Fix issue #28. Rewrite the program with SOLID
    The rule is Liskov substitution principle

---
+++ solutions/mskat/issue#28/src/main/java/Magpie.java
@@ -0,0 +1,9 @@
+public class Magpie implements Bird, FlyingBird {
+    public void eat() {
+        System.out.println("Eating");
+    }
+
+    public void fly() {
+        System.out.println("Flying");
+    }
+}
---
+++ solutions/mskat/issue#28/src/main/java/Penguin.java
@@ -0,0 +1,5 @@
+public class Penguin implements Bird {
+    public void eat() {
+        System.out.println("Eating");
+    }
+}
